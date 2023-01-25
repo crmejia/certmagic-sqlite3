86667c5bed98b384b14c99fba670f0014222f0d9
Crismar Mejia <crismar.mejia@gmail.com>
2023-01-25 13:07:49 -0400

    adding delete and exist

--- storage.go
+++ storage.go
@@ -12,14 +12,21 @@ type storage struct {
 	db *sql.DB
 }
 
-func OpenSQLiteStorage(dataSourceName string)(storage,error){
+func OpenSQLiteStorage(dataSourceName string) (storage, error) {
 	if dataSourceName == "" {
-	return storage{}, errors.New("data source cannot be empty")
+		return storage{}, errors.New("data source cannot be empty")
 	}
 
-	db, err := sql.Open("sqlite",dataSourceName)
+	db, err := sql.Open("sqlite", dataSourceName)
 	if err != nil {
 		return storage{}, err
+	}
+
+	for _, stmt := range []string{pragmaWALEnabled, pragma500BusyTimeout } {
+		_, err = db.Exec(stmt, nil)
+		if err != nil {
+			return storage{}, err
+		}
 	}
 
 	_, err = db.Exec(createTable)
@@ -31,17 +38,17 @@ func OpenSQLiteStorage(dataSourceName string)(storage,error){
 		db: db,
 	}
 	return s, nil
-	
+
 }
 
-func(s *storage)Store(ctx context.Context, key string, value []byte) error{
+func (s *storage) Store(ctx context.Context, key string, value []byte) error {
 	if key == "" {
 		return errors.New("key cannot be empty")
 	}
 	if len(value) == 0 {
 		return errors.New("value cannot be empty")
 	}
-		stmt, err := s.db.Prepare(insertKey)
+	stmt, err := s.db.Prepare(insertKey)
 	if err != nil {
 		return err
 	}
@@ -53,30 +60,62 @@ func(s *storage)Store(ctx context.Context, key string, value []byte) error{
 	return nil
 }
 
-func(s *storage)Load(ctx context.Context, key string)([]byte,error){
+func (s *storage) Load(ctx context.Context, key string) ([]byte, error) {
 	rows, err := s.db.Query(getKEY, key)
 	if err != nil {
 		return nil, err
 	}
 	value := []byte{}
-	for rows.Next(){
+	for rows.Next() {
 		err = rows.Scan(&value)
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	if err = rows.Err(); err !=nil{
+	if err = rows.Err(); err != nil {
 		return nil, err
 	}
 	return value, nil
 }
 
+func (s *storage) Exists(ctx context.Context, key string) bool{
+	rows, err := s.db.Query(getKEY, key)
+	if err != nil {
+		return false
+	}
+	if rows.Next() {
+		return true
+	}
+	return false
+}
+
+func (s *storage) Delete(ctx context.Context, key string) error{
+	if key == "" {
+		return errors.New("key cannot be empty")
+	}
+	stmt, err := s.db.Prepare(deleteKey)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(key)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 const createTable = `
 CREATE TABLE IF NOT EXISTS certmagic(
-	key TEXT NOT NULL,
+	key TEXT NOT NULL PRIMARY KEY,
 	value BLOB NOT NULL);`
+
+const pragmaWALEnabled = `PRAGMA journal_mode = WAL;`
+const pragma500BusyTimeout = `PRAGMA busy_timeout = 5000;`
 
 const insertKey = `INSERT into certmagic(key, value) VALUES (?, ?);`
 
 const getKEY = `SELECT value from certmagic WHERE key = ?`
+
+const deleteKey = `DELETE FROM certmagic WHERE key=?`
--- storage_test.go
+++ storage_test.go
@@ -6,9 +6,9 @@ import (
 
 	"github.com/crmejia/certmagic-sqlite3"
 	"github.com/google/go-cmp/cmp"
-)	
+)
 
-func TestStorage_RoundtripStoreLoad(t *testing.T){
+func TestStorage_RoundtripStoreLoadExistsDelete(t *testing.T) {
 	t.Parallel()
 	tempDB := t.TempDir() + "roundtrip.db"
 	storage, err := certmagicsqlite3.OpenSQLiteStorage(tempDB)
@@ -18,13 +18,60 @@ func TestStorage_RoundtripStoreLoad(t *testing.T){
 	ctx := context.Background()
 	key := "certkey"
 	want := []byte("certificate")
+
+	exists := storage.Exists(ctx, key)
+	if exists {
+		t.Error("key should not exist")
+	}
+
+	err = storage.Delete(ctx, key)
+	if err != nil {
+		t.Error("want no error on delete non-existing key")
+	}
+
 	err = storage.Store(ctx, key, want)
 	if err != nil {
 		t.Fatal(err)
 	}
-	
-	got, err := storage.Load(ctx,key)
+
+	got, err := storage.Load(ctx, key)
 	if !cmp.Equal(want, got) {
 		t.Errorf("want value to be %s, got %s", want, got)
 	}
+
+	exists = storage.Exists(ctx, key)
+	if !exists {
+		t.Error("want key to exist")
+	}
+
+	err = storage.Delete(ctx, key)
+	if err != nil {
+		t.Error("want no error on delete existing key")
+	}
+	
+	exists = storage.Exists(ctx, key)
+	if exists {
+		t.Error("key should not exist")
+	}
 }
+
+// func TestDeleteKey(t *testing.T){
+// 	t.Parallel()
+// 	tempDB := t.TempDir() + "delete.db"
+// 	storage, err := certmagicsqlite3.OpenSQLiteStorage(tempDB)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	ctx := context.Background()
+// 	key := "certkey"
+// 	want := []byte("certificate")
+// 	err = storage.Delete(ctx, key)
+// 	if err != nil {
+// 		t.Error("want no error on delete non-existing key")
+// 	}
+//
+// 	err = storage.Store(ctx, key, want)
+// 	if err != nil {
+// 		t.Error("want no error on store")
+// 	}
+// }
